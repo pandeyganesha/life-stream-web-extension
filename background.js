7337c2d3a5a022472af3679f2cf6c97ce6a96586
pandeyganesha <pandeyganesha@gmail.com>
2026-08-15 17:46:01 +0530

    feat: updated OnFocusChanged event as well
    - it still seem to have some bugs, where end_time is not getting set in some cases

--- background.js
+++ background.js
@@ -1,35 +1,9 @@
 console.log("Background script loaded.");
 
-TIME_SPENT_MAP = new Map();
-
+const TIME_SPENT_MAP = new Map();
+let active_site = null;
 let previous_site = null;
-let previous_site_start_time = null;
-
-function update_time_spent_map(active_site) {
-    
-    // do not calculate again, if old and new sites are same
-    if (active_site === previous_site)
-        return;
-
-    // Save timestamp when the site started
-    active_site_start_time = Date.now();
-    
-    // Set the key in map if not present
-    if (!TIME_SPENT_MAP.has(active_site)) {
-        TIME_SPENT_MAP.set(active_site, 0);
-    }
-
-    // Calculate time spent for previous tab
-    if (previous_site !== null) {
-        const previous_time = TIME_SPENT_MAP.get(previous_site);
-        const time_spent = Date.now() - previous_site_start_time;
-        TIME_SPENT_MAP.set(previous_site, previous_time + time_spent);
-    }
-
-    // active site becomes previous site ( eventually )
-    previous_site = active_site
-    previous_site_start_time = active_site_start_time;
-}
+let last_focused_window_id;
 
 async function get_url_from_tab_id(tab_id) {
     const tab = await browser.tabs.get(tab_id);
@@ -38,15 +12,64 @@ async function get_url_from_tab_id(tab_id) {
     return url.hostname ? url.hostname : "Empty-Tab";
 }
 
-browser.tabs.onActivated.addListener(async (active_info) => {
-    console.log("On Activated")
-    update_time_spent_map(await get_url_from_tab_id(active_info.tabId))
-    console.log("TIME_SPENT_MAP:", TIME_SPENT_MAP);
+function set_start_timestamp_for(site){
+    if (!TIME_SPENT_MAP.has(site)){
+        TIME_SPENT_MAP.set(site, [])
+    }
+    TIME_SPENT_MAP.get(site).push({"start_time": Date.now()});
+    console.log(`START: ${JSON.stringify([...TIME_SPENT_MAP], null, 2)}`);
+}
 
+function set_end_timestamp_for(site){
+    TIME_SPENT_MAP.get(site).at(-1).end_time = Date.now();
+    console.log(`START: ${JSON.stringify([...TIME_SPENT_MAP], null, 2)}`);
+}
+
+function get_time_spent_for(site){
+
+}
+
+function get_time_spent_summary(){
+
+}
+
+browser.tabs.onActivated.addListener(async (active_info) => {
+    console.log("On Activated");
+    active_site = await get_url_from_tab_id(active_info.tabId);
+    set_start_timestamp_for(active_site);
+    if (previous_site)
+        set_end_timestamp_for(previous_site);
+
+    previous_site = active_site;
 });
 
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    console.log("On Updated")
-    update_time_spent_map(await get_url_from_tab_id(tabId))
-    console.log("TIME_SPENT_MAP:", TIME_SPENT_MAP);
+    console.log("On Updated");
+    let new_active_site = await get_url_from_tab_id(tabId);
+    if (new_active_site === active_site)
+        return;
+    active_site = new_active_site;
+    set_start_timestamp_for(active_site);
+    set_end_timestamp_for(previous_site);
+    previous_site = active_site;
+});
+
+browser.windows.onFocusChanged.addListener(async (windowId) => {
+    console.log("On Window Focus Changed")
+
+    if (windowId == -1){
+        const [tab_to_update] = await browser.tabs.query({
+            active: true,
+            windowId: last_focused_window_id
+        });
+        set_end_timestamp_for(await get_url_from_tab_id(tab_to_update.id));
+    }
+    else {
+        last_focused_window_id = (await browser.windows.getCurrent()).id;
+        const [tab_to_update] = await browser.tabs.query({
+            active: true,
+            windowId: windowId
+        });
+        set_start_timestamp_for(await get_url_from_tab_id(tab_to_update.id));
+    }
 });
